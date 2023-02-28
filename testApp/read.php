550385257d38b7b0a0e27ca436e9b256c198fe2d
Ranjith Menon <60739917+theranjithmenon@users.noreply.github.com>
2023-02-28 21:17:51 +0530

    Add files via upload

---
+++ testApp/read.php
@@ -0,0 +1,20 @@
+<?php
+include 'connect.php';
+
+$sql = $conn -> query("SELECT * FROM userdetails");
+$list = array();
+if($sql -> num_rows > 0){
+    while ($row = $sql -> fetch_assoc()) {
+        $myarray['name'] = $row['user_name'];
+        $myarray['email'] = $row['user_email'];
+        $myarray['phone'] = $row['user_ph'];
+        $myarray['id'] = $row['id'];
+        array_push($list,$myarray);
+    }
+}
+else{
+    $myarray['result'] = 'failed';
+    array_push($list,$myarray);
+}
+echo json_encode($list);
+?>
